dc5d1d147a247d7c65ad0ab2b54f91ac85b11cb4
hetongxue303 <1974597686@qq.com>
2022-09-05 13:47:36 +0800

    configure environment, styles, vite and ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/stytle/global.scss'
+import 'reset.css'
+import 'animate.css'
+import 'virtual:windi.css'
+
+const app = createApp(App)
+
+app.mount('#app')
